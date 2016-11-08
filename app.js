d85675d1caf6aea8e701472a33a2537685a6c40c
Jasmichele <jasmichele@yahoo.com>
2016-11-08 12:53:42 -0500

    12:53 11/8/16

--- app.js
+++ app.js
@@ -22,4 +22,19 @@ elem3.innerHTML = 'Today is ' + month + '/' + dd + '/' + year;
 
 var money = '&#36;' + 5.45;
 var elem4 = document.getElementById('demo4');
-elem4.innerHTML = money;
+elem4.innerHTML = 'I spent ' + money + ' on a cheeseburger';
+
+var letter = 'X';
+var elem5 = document.getElementById('demo5');
+elem5.innerHTML = letter + ' marks the spot';
+
+var movies = ['Pretty Woman ', ' Friday ', ' Blow ', ' Coming To America'];
+document.getElementById('demo6').innerHTML = movies;
+
+movies.push(' Mo Money');
+
+document.getElementById('demo7').innerHTML = movies;
+
+delete movies[1];
+document.getElementById('demo8').innerHTML = movies;
+
